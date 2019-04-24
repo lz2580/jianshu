6f83d7436d27f6bc2f849a3dcc4563db97fc02ba
lz2580 <496820337@qq.com>
2019-04-24 19:59:54 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React,{ Component } from 'react';
+import React,{ Component,Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './style';
 import {IconFont} from './static/iconfont/iconfont'
+import {BrowserRouter,Route} from "react-router-dom";
 import Header from './common/heard'
 import store from './store'
 class App extends Component{
 	render() {
 		return (
 			<Provider store={store}>
+				<Fragment>
 				<GlobalStyle/>
 				<IconFont/>
 				<Header/>
+				<BrowserRouter>
+					<div>
+						<Route path='/' exact render={() => <div>首页</div>}></Route>
+						<Route path='/detail' exact render={() => <div>detail</div>}></Route>
+					</div>
+				</BrowserRouter>
+				</Fragment>
 			</Provider>
 		)
 	}
